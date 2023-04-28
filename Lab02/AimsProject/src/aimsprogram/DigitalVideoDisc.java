04b1760c6e7dba02502085969af68bbd66f9ab2d
tientu2003 <117565347+tientu2003@users.noreply.github.com>
2023-04-28 11:35:36 +0700

    hmm

--- Lab02/AimsProject/src/aimsprogram/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/aimsprogram/DigitalVideoDisc.java
@@ -1,6 +1,21 @@
 package aimsprogram;
 
 public class DigitalVideoDisc {
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
 	private String title;
 	private String category;
 	private String director;
