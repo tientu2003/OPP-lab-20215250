ae083da0f2faf8a304554dfc4f098b3adc62e3f4
tientu2003 <117565347+tientu2003@users.noreply.github.com>
2023-05-14 08:15:42 +0700

    5.class member

--- Lab03/AIMS/src/aimsprogram/Aims.java
+++ Lab03/AIMS/src/aimsprogram/Aims.java
@@ -10,32 +10,26 @@ public class Aims {
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-		
-		//DigitalVideoDisc dvdList[] = {dvd1,dvd2,dvd3};
-		
-		// Add the list of DVDs
-		//aCart.addDigitalVideoDisc(dvdList);
-		// Add two List of DVDs
-		aCart.addDigitalVideoDisc(dvd2,dvd3);
-		
-		//print total cost of items in the cart
-	
-		System.out.printf("Total cost is: %.2f\n",aCart.totalCost());
+		DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
 		
 		
-		
-		
-		
-//		// Check the remove method
-//		System.out.println("-----------------------------");
-//		DigitalVideoDisc dvd4 = new DigitalVideoDisc("The Lord of the Rings","Action",35.39f);
-//		aCart.addDigitalVideoDisc(dvd4);
+//		// Part 2 Method overloading
+//		DigitalVideoDisc dvdList[] = {dvd1,dvd2,dvd3};
+//		// Add the list of DVDs
+//		aCart.addDigitalVideoDisc(dvdList);
+//		// Add two List of DVDs
+//		aCart.addDigitalVideoDisc(dvd2,dvd3);
 //		
-//		aCart.removeDigitalVideoDisc(dvd1);
-//		aCart.removeDigitalVideoDisc(dvd4);
-//		
-//		// Check the remaining items in Cart
-//		aCart.checkquantity();
+//		//print total cost of items in the cart
+//		System.out.printf("Total cost is: %.2f\n",aCart.totalCost());
+		
+		
+		//Part 5 Class member
+		System.out.println(dvd1.getId() +" " + dvd1.getTitle());
+		System.out.println(dvd3.getId() +" " + dvd3.getTitle());
+		
+		System.out.println("The number of DVDs created: " + DigitalVideoDisc.getNbDigitalVideoDiscs());
+		
 	}	
 	
 }
